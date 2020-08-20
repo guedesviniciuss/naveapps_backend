24bd72d786ebc8390266c7874e0f7e014a024aec
Viniciusguedes <viniciusguedes@lavid.ufpb.br>
2020-08-20 00:47:43 -0300

    cors added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import routes from './routes';
 import './database';
 
@@ -8,6 +9,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
